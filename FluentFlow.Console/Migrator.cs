a1b9e423ea632f984be687d37ac3802299b6d232
Piotr Ferenc <ferenc.piotr@outlook.com>
2025-03-27 16:36:21 +0100

    refactor: fix formatting and improve syntax consistency
    Standardize formatting in project files and code for improved readability and maintainability. Adjusted indentation in various project files and corrected minor syntax inconsistencies in the `Migrator` class code.

--- FluentFlow.Console/Migrator.cs
+++ FluentFlow.Console/Migrator.cs
@@ -22,15 +22,16 @@ public static class Migrator
         _ => throw new DatabaseProviderNotSupportException(name)
     };
 
-    private static readonly Func<IDatabaseProvider, MigrationOptions, bool> BuildConfig = (provider, options) => GetDatabase!(provider, options) | GetTables!(provider,options);
+    private static readonly Func<IDatabaseProvider, MigrationOptions, bool> BuildConfig = (provider, options) => GetDatabase!(provider, options) | GetTables!(provider, options);
 
     private static readonly Func<IDatabaseProvider, MigrationOptions, bool> GetTables = (provider, options) =>
     {
         var tables = provider.GetTables(new Database(new Name(options.DatabaseName))).GetAwaiter().GetResult();
-var selectedTable = AnsiConsole.Prompt(new SelectionPrompt<string>().Title("Select table").MoreChoicesText("[grey](Move up and down to reveal more choices)[/]").AddChoices(tables.Select(x => x.Name.Value)));
-options.TableName = selectedTable;
-return true;
+        var selectedTable = AnsiConsole.Prompt(new SelectionPrompt<string>().Title("Select table").MoreChoicesText("[grey](Move up and down to reveal more choices)[/]").AddChoices(tables.Select(x => x.Name.Value)));
+        options.TableName = selectedTable;
+        return true;
     };
+
     private static readonly Func<IDatabaseProvider, MigrationOptions, bool> GetDatabase = (provider, options) =>
     {
         var databases = provider.GetDatabases().GetAwaiter().GetResult();
