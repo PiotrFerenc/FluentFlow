36f715a48db7d15253189d4438032f0c158a215b
Piotr Ferenc <ferenc.piotr@outlook.com>
2025-04-04 09:55:18 +0200

    feat: generate migration classes with Up and Down methods
    Added support for generating migration classes using a `ClassBuilder` with `Up` and `Down` methods. The `Up` method applies migration steps, while the `Down` method provides rollback logic. Code is formatted using `NormalizeWhitespace` for consistency.

--- FluentFlow.Console/Migrator.cs
+++ FluentFlow.Console/Migrator.cs
@@ -5,6 +5,8 @@ using FluentFlow.Core.Code;
 using FluentFlow.Provider;
 using FluentFlow.Provider.Postgres;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Spectre.Console;
 using Name = FluentFlow.Provider.Name;
 using Table = FluentFlow.Provider.Table;
@@ -16,12 +18,13 @@ public static class Migrator
     public static void Main(Options options)
     {
         var provider = GetProvider(options.Provider);
-       var connection =  provider.TryConnect(new ConnectionString(options.ConnectionString)).GetAwaiter().GetResult();
-       if (connection.IsFailed)
-       {
-           AnsiConsole.MarkupLine($"[red]Error:[/] {connection.Error}");
-           return;
-       }
+        var connection = provider.TryConnect(new ConnectionString(options.ConnectionString)).GetAwaiter().GetResult();
+        if (connection.IsFailed)
+        {
+            AnsiConsole.MarkupLine($"[red]Error:[/] {connection.Error}");
+            return;
+        }
+
         var migrationOptions = new MigrationOptions();
         if (BuildConfig(provider, migrationOptions))
         {
@@ -32,7 +35,44 @@ public static class Migrator
                     .AddStep(ColumnMapper.Map(column.Type.Value));
             }
 
-            System.Console.WriteLine(migration.Build().NormalizeWhitespace());
+            var code = ClassBuilder.Build(x =>
+            {
+                x.Name = $"{migrationOptions.TableName}Migration";
+                x.Inheritance = "Migration";
+                x.Attributes = [ClassAttributeBuilder.Build("Migration", AttributeArgument.Build((long.Parse(DateTime.Now.ToString("yyyyMMddHHmm")))))];
+                x.Methods =
+                [
+                    MethodBuilder.Build(m =>
+                    {
+                        m.Name = "Up";
+                        m.Modifiers =
+                        [
+                            SyntaxKind.PublicKeyword,
+                            SyntaxKind.OverrideKeyword
+                        ];
+                        m.Body = Method.Body(migration.Build());
+                        m.ReturnType = "void";
+                    }),
+
+                    MethodBuilder.Build(m =>
+                    {
+                        m.Name = "Down";
+                        m.Modifiers =
+                        [
+                            SyntaxKind.PublicKeyword,
+                            SyntaxKind.OverrideKeyword
+                        ];
+                        m.ReturnType = "void";
+                        m.Body = Method.Body(
+                            new FluentBuilder("Delete", true)
+                                .AddStep("Table", Argument.String(migrationOptions.TableName))
+                                .Build()
+                        );
+                    })
+                ];
+            });
+
+            System.Console.WriteLine(code.NormalizeWhitespace());
         }
     }
 
