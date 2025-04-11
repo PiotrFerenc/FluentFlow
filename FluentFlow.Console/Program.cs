e955829a2638203ea48670a8d74487a4b55cc64c
Piotr Ferenc <ferenc.piotr@outlook.com>
2025-04-11 13:39:58 +0200

    refactor: remove unused using directives
    Removed unnecessary `using` statements from PostgresDatabaseProvider and Program files to clean up the codebase. This improves readability and reduces clutter without impacting functionality.

--- FluentFlow.Console/Program.cs
+++ FluentFlow.Console/Program.cs
@@ -1,6 +1,5 @@
 ﻿using CommandLine;
 using FluentFlow.Console;
 using FluentFlow.Console.Model;
-using Microsoft.Extensions.DependencyInjection;
 
 Parser.Default.ParseArguments<Options>(args).WithParsed(Migrator.Main);
--- FluentFlow.Provider.Postgres/PostgresDatabaseProvider.cs
+++ FluentFlow.Provider.Postgres/PostgresDatabaseProvider.cs
@@ -1,5 +1,4 @@
 using System.Data;
-using System.Data.Common;
 using FluentFlow.Core;
 using Npgsql;
 
